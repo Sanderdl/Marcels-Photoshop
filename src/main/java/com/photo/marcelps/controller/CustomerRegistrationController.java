66c505622564703e4381321294e794cb098577b5
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-03-06 15:31:27 +0100

    added customer controller

---
+++ src/main/java/com/photo/marcelps/controller/CustomerRegistrationController.java
@@ -0,0 +1,11 @@
+package com.photo.marcelps.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Tomt on 6-3-2017.
+ */
+@Controller
+public class CustomerRegistrationController {
+
+}
