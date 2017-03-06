d4e940f87f3b51e0eb5251994fec696e3cb8855d
Visser,Adriaan A.A. de <a.devisser@student.fontys.nl>
2017-03-06 16:05:23 +0100

    Added Mysql singleton for opening and closing connection

---
+++ src/main/java/data/database/MySQLDatabase.java
@@ -0,0 +1,70 @@
+package data.database;
+
+import java.io.IOException;
+import java.sql.*;
+
+/**
+ * Created by Adriaan on 06-Mar-17.
+ */
+public class MySQLDatabase {
+
+    private final static String connectionString = "jdbc:mysql://web401.your-webhost.nl:3306/qb401179_Teken";
+    private final static String user = "qb401179_sander";
+    private final static String password = "1234";
+    public final static MySQLDatabase dbConnection = new MySQLDatabase();
+
+    public MySQLDatabase(){
+        if(dbConnection == null){
+            initConnection();
+        }
+    }
+
+    public Connection getConnection() throws SQLException, IOException {
+
+        try {
+            Connection conn = DriverManager.getConnection(connectionString, user, password);
+            System.out.println("Connected to MySql: " + conn.toString());
+            return conn;
+
+        }
+        catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            throw ex;
+        }
+    }
+
+    public void closeConnection(Connection con) {
+        try {
+            con.close();
+        }
+        catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+        }
+    }
+
+    public void closeConnection(Connection con, PreparedStatement stm, ResultSet rs){
+        try {
+            rs.close();
+            stm.close();
+            con.close();
+        }
+        catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+        }
+    }
+
+    private void initConnection() {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
