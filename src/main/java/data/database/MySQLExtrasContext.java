c16745012295bbbfaf3efeb23a833788e7229c16
i340824 <s.delaat@student.fontys.nl>
2017-03-13 16:00:33 +0100

    Added get extras

---
+++ src/main/java/data/database/MySQLExtrasContext.java
@@ -0,0 +1,7 @@
+package data.database;
+
+/**
+ * Created by sande on 13/03/2017.
+ */
+public class MySQLExtrasContext {
+}
---
+++ src/main/java/data/database/interfaces/IExtrasContext.java
@@ -0,0 +1,7 @@
+package data.database.interfaces;
+
+/**
+ * Created by sande on 13/03/2017.
+ */
+public interface IExtrasContext {
+}
