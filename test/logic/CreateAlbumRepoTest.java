03c9fc2cbad811dffe134860e32bdb6e28b7c588
Ruud van den Boomen <ruudvdboomen@hotmail.com>
2017-03-20 13:16:36 +0100

    Album repo test

---
+++ test/logic/CreateAlbumRepoTest.java
@@ -0,0 +1,41 @@
+package logic;
+
+import models.Album;
+import models.Photographer;
+import models.User;
+import models.exceptions.UploadException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ruudv on 20-3-2017.
+ */
+public class CreateAlbumRepoTest {
+
+    private CreateAlbumRepo createAlbumRepo;
+    private Photographer user;
+    private Album album;
+
+    @Before
+    public void setup() {
+        this.createAlbumRepo = new CreateAlbumRepo();
+        this.user = new Photographer(1, "Fred", "Fred Fransen", "Fred@Fred.nl", User.UserStatus.verified);
+        this.album = new Album("TestAlbum", user, 1);
+    }
+
+    @Test
+    public void validateAlbumNameTest() throws UploadException, SQLException {
+        try {
+            this.album.setName("");
+            createAlbumRepo.validateUploadAlbum(album, user);
+        } catch (final UploadException e) {
+            final String msg = "Invalid album name";
+            assertEquals(msg, e.getMessage());
+        }
+    }
+
+}
