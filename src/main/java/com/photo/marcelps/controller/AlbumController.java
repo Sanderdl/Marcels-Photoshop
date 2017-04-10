f82375390768c4a7f55905937f5873d4909da489
i340824 <s.delaat@student.fontys.nl>
2017-04-10 12:29:03 +0200

    Load album images on album page

--- src/main/java/com/photo/marcelps/controller/AlbumController.java
+++ src/main/java/com/photo/marcelps/controller/AlbumController.java
@@ -1,10 +1,13 @@
 package com.photo.marcelps.controller;
 
+import logic.AlbumRepo;
 import models.GalleryImage;
+import models.exceptions.AlbumException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
@@ -16,14 +19,34 @@ import java.util.Map;
  * Created by sande on 10/04/2017.
  */
 @Controller
-@RequestMapping("Album")
+@RequestMapping("album")
 public class AlbumController {
 
+    private AlbumRepo repo = new AlbumRepo();
+
     @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ModelAndView showImage(@RequestParam("id") int id, HttpSession session) {
+
+        Map<Integer, GalleryImage> map = null;
+
+        try {
+            map = repo.retrieveAlbumPictures(id);
+        } catch (AlbumException e) {
+            e.printStackTrace();
+        }
+
+        ModelAndView model = new ModelAndView("album");
+        model.addObject("map", map);
+        session.setAttribute("map",map);
+
+        return model;
+    }
+
+    @RequestMapping(value = "/image", method = RequestMethod.GET)
     public void showImage(@RequestParam("id") int id, HttpServletResponse response, HttpSession session)
             throws ServletException, IOException {
 
-        Map<Integer, GalleryImage> map = (Map<Integer, GalleryImage>)session.getAttribute("Gallery");
+        Map<Integer, GalleryImage> map = (Map<Integer, GalleryImage>)session.getAttribute("map");
         response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
         response.getOutputStream().write(map.get(id).getImage());
         response.getOutputStream().close();
--- src/main/webapp/WEB-INF/views/album.jsp
+++ src/main/webapp/WEB-INF/views/album.jsp
@@ -1,16 +1,42 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: sande
-  Date: 10/04/2017
-  Time: 09:30
-  To change this template use File | Settings | File Templates.
---%>
+<!DOCTYPE HTML>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Album</title>
+    <title>Marcel's Photo Shop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<c:url value="/resources/css/justifiedGallery.min.css"/>"/>
+    <link rel="stylesheet" href="<c:url value="/resources/css/style.css"/>"/>
+    <link rel="stylesheet" href="<c:url value="/resources/css/bootstrap.css"/>"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="<c:url value="/resources/js/jquery-3.1.1.min.js"/>"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<%@ include file="header.jsp" %>
+<section id="photos" class="justified-gallery shadow-m">
+    <c:if test="${not empty map}">
+        <c:forEach var="listValue" items="${map}">
+            <a href="/viewphoto/page/?id=${listValue.key}">
+                <img src="/album/image?id=${listValue.key}">
+            </a>
+        </c:forEach>
+    </c:if>
+</section>
 
 </body>
+<script src="<c:url value="/resources/js/jquery.justifiedGallery.min.js"/>"></script>
+<script>
+    $(function () {
+        $("#photos").justifiedGallery({
+            rowHeight: 130,
+            maxRowHeight: -1,
+            margins: 6,
+            captions: true
+        });
+    });
+</script>
 </html>
+
