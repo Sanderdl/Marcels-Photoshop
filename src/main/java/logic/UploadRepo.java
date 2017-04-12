05ef7c807a5196edfd2b4a00b3adbbc4474dfce5
lucreinink <lucreinink@icloud.com>
2017-04-12 11:50:41 +0200

    Import toegevoegd.

--- src/main/java/logic/UploadRepo.java
+++ src/main/java/logic/UploadRepo.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
